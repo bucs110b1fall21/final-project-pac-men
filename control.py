b384e901c38dac5e245cd03b4db7f048584e4dd4
AxinLi1 <ali166@binghamton.edu>
2021-12-12 01:00:01 -0500

    Fixed game over event

--- control.py
+++ control.py
@@ -10,7 +10,7 @@ class Controller:
 
     def __init__(self):
         ''' Initializes the game the sprites Pacman and the Ghosts. Sets the default value of
-         background as 600x700 and displays the maze.png. Also Pacman will always be in a moving state until the game ends
+         background as 610x670 and displays the maze.png. Also Pacman will always be in a moving state until the game ends
          with self.running. '''
         pacman = player.Pacman
         ghosts = enemies.Ghosts
@@ -51,24 +51,9 @@ class Controller:
         for i in range(config.SCREEN_WIDTH // self.maze_width):
             pygame.draw.line(self.background, config.WHITE, (i * self.maze_width, 0),
                              (i * self.maze_height, config.SCREEN_HEIGHT))
-        for i in range(config.SCREEN_HEIGHT // self.box_height):
-            pygame.draw.line(self.background, config.WHITE, (0, i * self.box_height),
-                             (config.SCREEN_WIDTH, i * self.box_height))
-
-    '''def makeEnemies(self):
-        for idx, pos in enumerate(self.ghostpositions):
-            self.ghosts.append(enemies.Ghosts(self, vec(pos), idx))'''
-
-    def reset(self):
-        '''Resets the game once Pacman touches a Ghost.'''
-        self.pacman.lives = 1
-        self.pacman.coordinates = vec(self.pacman.starting_pos)
-        self.pacman.pixel_position = self.pacman.getPosition()
-        self.pacman.direction *= 0
-        self.pacman.grid_pos = vec(self.pacman.starting_pos)
-        '''for ghost in self.ghosts:
-            ghost.grid_pos = vec(enemies.Ghosts.starting_pos)
-            ghost.pixel_position = ghost.getPosition()'''
+        for i in range(config.SCREEN_HEIGHT // self.maze_height):
+            pygame.draw.line(self.background, config.WHITE, (0, i * self.maze_height),
+                             (config.SCREEN_WIDTH, i * self.maze_height))
 
     def mainloop(self):
         '''Sets up the three game states, menu, playing the game, and the end screen. Currently
@@ -77,15 +62,14 @@ class Controller:
             if self.state == "Initialization":
                 self.menuEvents()
                 self.menuDraw()
-                self.drawMaze()
+                # self.drawMaze()
             elif self.state == "In-Game":
                 self.gameEvents()
                 self.gameUpdate()
                 self.gameDraw()
             elif self.state == "Game-Over":
+                self.gameoverevent()
                 self.gameoverloop()
-            else:
-                self.running = False
         pygame.quit()
         sys.exit()
 
@@ -105,21 +89,13 @@ class Controller:
             800 // 2 - 170, 600 // 2 - 50], 15, (170, 132, 58), "arial black", centered=True)
         pygame.display.update()
 
-    def type(self, characters, screen, pos, size, color, font_name, centered=True):
-        '''Method used for the pygame.type function'''
+    def type(self, characters, screen, position, size, color, font_name, centered=True):
+        '''Method used for the pygame.type function and displays the characters on the screen given a position, font size, font color,
+        and font'''
         font = pygame.font.SysFont(font_name, size)
         text = font.render(characters, False, color)
         text_size = text.get_size()
-        screen.blit(text, pos)
-
-    def drawText(self, txt, screen, pos, size, color, font, centered=False):
-        font = pygame.font.SysFont(font, size)
-        text = font.render(txt, False, color)
-        text_size = text.get_size()
-        if centered:
-            pos.x = pos.x - text_size[0] // 2
-            pos.y = pos.y - text_size[1] // 2
-        screen.blit(text, pos)
+        screen.blit(text, position)
 
     def gameEvents(self):
         '''Creates the movement of Pacman with arrow keys'''
@@ -135,51 +111,52 @@ class Controller:
                     self.pacman.move(vec(0, 1))
 
     def drawCoins(self):
+        '''Draws coins on the background using the maze (not drawn) and places the coins according to positions on the grid.'''
         for coins in self.coins:
             pygame.draw.circle(self.screen, (124, 123, 7),
-                               (int(coins.x * self.maze_width) + self.maze_width // 2 + config.TOP_BOTTOM_BUFFER // 2,
-                                int(coins.y * self.maze_height) + self.maze_height // 2 + config.TOP_BOTTOM_BUFFER // 2),
+                               (int(coins.x * self.maze_width) + self.maze_width // 2 + config.EMPTY_SPACE // 2,
+                                int(coins.y * self.maze_height) + self.maze_height // 2 + config.EMPTY_SPACE // 2),
                                5)
 
     def gameUpdate(self):
-        '''Updates the movement of Pacman'''
+        '''Updates the movement of Pacman and the ghost'''
         self.pacman.update()
-        # for i in range(4):
-        # self.ghost.update()
         self.ghost.update()
-        '''Checks if the ghost coordinates is equal to Pacman's coordinates, if so, Pacman loses a life.'''
+        '''Checks if the ghost coordinates is equal to Pacman's coordinates, if so, Pacman loses a life and ends the game state. 
+        This starts the 'Game-Over' state.'''
         if self.pacmanposition == self.ghostpositions:
             self.loseLife()
 
     def gameDraw(self):
-        '''Draws Pacman and the game screen'''
+        '''Draws Pacman, the game screen, coins on the board, and also draws the SCORE: text on the top left of the game window.'''
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.background, (25, 25))
         self.pacman.draw()
         self.drawCoins()
-        self.drawText('SCORE: {}'.format(self.pacman.score),
-                      self.screen, [60, 0], 18, config.WHITE, config.FONT)
-        # for i in range(4):
-        # self.ghost.draw()
+        self.type('SCORE: {}'.format(self.pacman.score),
+                  self.screen, [60, 0], 18, config.WHITE, config.FONT)
+
         self.ghost.draw()
         pygame.display.update()
 
-    def loseCondition(self):
-        '''Initializes the Game-Over state of the game.'''
-        if self.pacman.lives == 0:
-            self.state = "Game-Over"
-
     def loseLife(self):
+        '''Pacman loses a life, thus ending the game and updates the game state to 'Game-Over'.'''
         self.pacman.lives -= 1
         if self.pacman.lives == 0:
             self.state = "Game-Over"
 
-    def gameoverloop(self):
-        '''Game over state oef the game'''
+    def gameoverevent(self):
+        "Game-Over state of the game. Stops Pacman from moving and pressing escape closes the game window."
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                self.reset()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESPACE:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.running = False
+
+    def gameoverloop(self):
+        '''Game over state of the game. Displays the text "GAME OVER"'''
+        self.screen.fill(config.BLACK)
+        self.type("GAME OVER", self.screen, [400 // 2, 600 // 2], 52,
+                  config.WHITE,
+                  "arial", centered=True)
+        pygame.display.update()
